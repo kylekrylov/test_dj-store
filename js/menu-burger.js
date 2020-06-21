b756c0d80039b08ad52a99fef34f98e6670ed0f2
kylerylov <kylerylov@gmail.com>
2020-06-21 22:25:50 +0300

    .02

---
+++ js/menu-burger.js
@@ -0,0 +1,28 @@
+"use strict";
+//
+$(document).ready(function() {
+    $('.header_burger ').click(function(menuburger) {
+        $('.header_burger,.header_menu-mob').toggleClass('active');
+        $('body').toggleClass('lock')
+            // $('.content').toggleClass('opacity')
+
+    });
+    $('.header_menu a').click(function(e) {
+        $('.header_burger,.header_menu-mob').removeClass('active');
+        $('body').removeClass('lock')
+            // $('.content').removeClass('opacity')
+    });
+
+});
+$(document).ready(function() {
+    $('.mob_contact button').click(function(modal) {
+        $('.header_burger,.header_menu-mob,.modal-pop').toggleClass('active');
+        // $('.content').removeClass('opacity')
+    });
+    $('.modal-pop button.close').click(function(e) {
+        $('.header_burger,.header_menu-mob,.modal-pop').removeClass('active');
+        $('body').removeClass('lock')
+            // $('.content').removeClass('opacity')
+    });
+
+});
