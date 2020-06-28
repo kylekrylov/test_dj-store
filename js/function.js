01432c1048ccb92c7f0ec0c55806dac2a7eb4c2b
kylerylov <kylerylov@gmail.com>
2020-06-28 23:01:03 +0300

    ///

--- js/function.js
+++ js/function.js
@@ -1,21 +1,23 @@
 "use strict";
 //
 $(document).ready(function() {
+    //вкл меню
     $('.header_burger ').click(function() {
         $('.header_burger,.header_menu-mob').toggleClass('active');
         $('body').toggleClass('lock')
-            // $('.content').toggleClass('opacity')
     });
-});
-$(document).ready(function() {
+    //активация окна из меню
     $('.mob_contact button').click(function() {
         $('.modal-pop').toggleClass('active');
         $('.header_burger,.header_menu-mob').removeClass('active');
     });
+    //закрытие по кнопки X 
     $('.close').click(function() {
         $('.modal-pop').removeClass('active');
+        $('.modal-ok').toggleClas('active');
         $('body').removeClass('lock')
     });
+
 });
 
 $(document).ready(function() {
